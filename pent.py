70a2f483f428d1aeac8762ed1fefe8cd7d5b37c9
ktf <not.discostoo@gmail.com>
2011-09-25 17:43:15 -0700

    make moves, sort of; check rows for winner

--- pent.py
+++ pent.py
@@ -12,7 +12,11 @@ FPS = 40
 
 # Player constants
 WHITE = 1
-BLACK = 0
+BLACK = -1
+
+# In pixels, of course
+BOARD_SIZE = 600
+BLOCK_SIZE = BOARD_SIZE / 6
 
 
 class Board(pygame.sprite.Sprite):
@@ -23,38 +27,95 @@ class Board(pygame.sprite.Sprite):
                [0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0] ]
 
+    _marbles = pygame.sprite.RenderPlain()
+    _block_width = BOARD_SIZE / 6
+
     def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface((600, 600))
+        self.image = pygame.Surface((BOARD_SIZE, BOARD_SIZE))
         self.image.fill(pygame.Color('red'))
         self.rect = self.image.get_rect(center=position)
 
-        # test...
-        self._board[0][2] = Marble(WHITE)
-        self._board[1][2] = Marble(BLACK)
-
     def update(self):
+        self._marbles.draw(self.image) # blit marbles onto game board
+
+    def make_move(self, color, mouse_coordinates):
+        """returns True if move is valid, False otherwise"""
+
+        # adjust screen coordinates to board coordinates
+        real_x, real_y = mouse_coordinates
+        real_x -= self.rect.left
+        real_y -= self.rect.top
+
+        # then to block coords, not pixels
+        col = real_x / BLOCK_SIZE
+        row = real_y / BLOCK_SIZE
+        return self._add_marble(color, row, col)
+
+    def winner(self):
+        """returns BLACK or WHITE or False"""
+        # TODO: also handle ties
+        return self._check_rows_for_winner() or\
+               self._check_cols_for_winner() or\
+               self._check_diags_for_winner()
+
+    def _add_marble(self, color, row, col):
+        """returns True if position is valid, False otherwise"""
+        if row > 5 or row < 0 or col > 5 or col < 0: # out of bounds
+            return False
+        if self._board[row][col]: # already a piece there
+            return False
+
+        topleft = (col * BLOCK_SIZE, row * BLOCK_SIZE)
+        new_marble = Marble(color, topleft)
+        self._marbles.add(new_marble) # add to sprite group
+        self._board[row][col] = new_marble # add to board array
+        return True
+
+    def _check_rows_for_winner(self):
+        """returns WHITE or BLACK or False"""
         for row in xrange(0, 6):
-            for col in xrange(0, 6):
-                if self._board[row][col]:
-                    if self._board[row][col].color == WHITE:
-                        pass
-                    else:
-                        pass
+            in_a_row = 1
+            for col in xrange(1, 6): # don't test first block
+                if self._check_equality(row, col, row, col - 1):
+                    in_a_row += 1
+                else:
+                    in_a_row = 1
+
+                if in_a_row >= 5:
+                    return self._board[row][col].color
+        return False
+
+    def _check_cols_for_winner(self):
+        # TODO
+        return False
+
+    def _check_diags_for_winner(self):
+        # TODO
+        return False
+
+    def _check_equality(self, row1, col1, row2, col2):
+        """helper function that makes checking for winners a bit cleaner"""
+        if self._board[row1][col1] and self._board[row2][col2]:
+            if self._board[row1][col1].color == self._board[row2][col2].color:
+                return True
+
+        return False
 
 
 class Marble(pygame.sprite.Sprite):
-    def __init__(self, color):
+    def __init__(self, color, position):
         pygame.sprite.Sprite.__init__(self)
 
+        # TODO: error checking
         self.color = color
-        self.image = pygame.Surface((80, 80))
+        self.image = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
         if self.color == WHITE:
             self.image.fill(pygame.Color('white'))
         else:
             self.image.fill(pygame.Color('black'))
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect(topleft=position)
 
 
 def main():
@@ -68,9 +129,10 @@ def main():
     all_sprites = pygame.sprite.RenderPlain()
 
     board = Board(screen.get_rect().center)
-    test_marble = Marble(WHITE)
     all_sprites.add(board)
-    all_sprites.add(test_marble)
+
+    # White goes first for now
+    current_player = WHITE
 
     # Main event loop
     while True:
@@ -79,21 +141,16 @@ def main():
             if event.type == pygame.QUIT:
                 shut_down(screen)
 
-            # Set the speed based on the key pressed
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     shut_down(screen)
 
-            # Reset speed when key goes up
-            if event.type == pygame.KEYUP:
-                pass
-
-            # Update the cursor position
-            if event.type == pygame.MOUSEMOTION:
-                pass
-
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                if board.make_move(current_player, pygame.mouse.get_pos()):
+                    winner = board.winner()
+                    if winner:
+                        print "Somebody won I guess"
+                    current_player = -current_player
 
         # Clear screen
         screen.fill(pygame.Color('black'))
